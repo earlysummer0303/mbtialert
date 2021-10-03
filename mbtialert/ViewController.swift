164b8aa6bfa4175877fc1b3745b252f1f6b92c43
earlysummer0303 <jw9914@naver.com>
2021-10-03 23:52:16 +0900

    Initial Commit

---
+++ mbtialert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mbtialert
+//
+//  Created by 황지우 on 2021/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
