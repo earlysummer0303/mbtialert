ea42200eb5b9f95c863649c3512d57a3d8712e19
earlysummer0303 <jw9914@naver.com>
2021-10-04 00:01:14 +0900

    first commit - first project

--- mbtialert/ViewController.swift
+++ mbtialert/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showEnfpAlert(_ sender: Any) {
+        
+                let enfpalert = UIAlertController(title:"엔프피",message:"나의 첫 엠비티아이",preferredStyle:.alert)
+                let enfpaction = UIAlertAction(title:"그렇구나~", style: .default, handler: nil)
+                enfpalert.addAction(enfpaction)
+                present(enfpalert,animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showIntpAlert(_ sender: Any) {
+        
+                let intpalert =
+                    UIAlertController(title:"인트피",message: "나의 지금 엠비티아이",preferredStyle: .alert)
+                let intpaction = UIAlertAction(title: "그렇구나~", style: .default, handler: nil)
+                intpalert.addAction(intpaction)
+                present(intpalert, animated: true, completion: nil)
+    }
+    
 }
 
